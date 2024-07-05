d299b71d4f8699d434f017c349737554ac14f29a
Luan8002Tr <lmateu446@gmail.com>
2024-07-05 14:59:04 +0000

    revisao

--- lab02/Entrada1.java
+++ lab02/Entrada1.java
@@ -2,7 +2,12 @@ package lab02;
 public class Entrada1 {
     // Não execute no 'Run'. Execute no Terminal
     // java 02-argumentos/Argumentos1.java romulo
+
     public static void main(String[] args) {
+
+        var aresta = Double.parseDouble(args[0]);
+        var volume = Math.pow(aresta, 3);
         System.out.println("Seu nome é " + args[0] );
+        
     }
 }
